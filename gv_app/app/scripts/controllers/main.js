ba5ca5feb5fc15379ab3d9c6c8e5850c8a85b1e7
liuweilong <viclong1993@gmail.com>
2014-11-16 15:44:01 +0800

    achieve the function in a stupid way.

--- gv_app/app/scripts/controllers/main.js
+++ gv_app/app/scripts/controllers/main.js
@@ -12,19 +12,4 @@ angular.module('gvApp')
     $scope.imgNames = [
     	'1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg'
     ];
-
-    $scope.expand = [];
-
-    $scope.len = $scope.imgNames.length;
-
-    while($scope.len--) $scope.expand.push(false);
-
-    $scope.expander = function(index) {
-    	$scope.expand[index] = !$scope.expand[index];
-    	for (var i = $scope.expand.length -1; i >= 0; i--) {
-    		if (i != index) {
-    			$scope.expand[i] = false;
-    		};
-    	};
-    }
   });
